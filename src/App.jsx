1491aed34421e7b31287e75d4fd4663fdd653c9a
SHAVIK821307 <shavikmmp@gmail.com>
2024-06-05 22:50:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ const App = () => {
   const [records, setRecords] = useState([]);
   const [updateUi, setUpdateUi] = useState(false);
 
-  // useEffect(() => {
-  //   const data = JSON.parse(localStorage.getItem("Money_Records")) || [];
-  //   setRecords(data);
-  // }, []);
-
   useEffect(() => {
     axios
       .get("http://localhost:3000/get")
@@ -23,7 +18,6 @@ const App = () => {
   const addRecord = (newRecord) => {
     const updatedRecords = [...records, newRecord];
     setRecords(updatedRecords);
-    // localStorage.setItem("Money_Records", JSON.stringify(updatedRecords));
 
     axios
       .post("http://localhost:3000/add", { task: newRecord })
@@ -35,11 +29,6 @@ const App = () => {
   };
 
   const updateRecords = (index, newdata) => {
-    // const newRecords = [...records];
-    // newRecords[index] = newdata;
-    // setRecords(newRecords);
-    // localStorage.setItem("Money_Records", JSON.stringify(newRecords));
-    // console.log(newdata);
     axios
       .put(`http://localhost:3000/update/${index}`, { task: newdata })
       .then((res) => {
@@ -50,10 +39,6 @@ const App = () => {
   };
 
   const deleteRecord = (id) => {
-    // const updatedRecords = records.filter((record, index) => index !== id);
-    // console.log(updatedRecords);
-    // setRecords(updatedRecords);
-    // localStorage.setItem("Money_Records", JSON.stringify(updatedRecords));
     axios
       .delete(`http://localhost:3000/delete/${id}`)
       .then((res) => {
